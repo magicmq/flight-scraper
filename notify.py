d9ad776ce380099e95bfbece065de527d7c3d2d8
Michael <mquindt@yahoo.com>
2024-11-22 14:15:34 -0600

    Replace print with log

--- notify.py
+++ notify.py
@@ -1,12 +1,15 @@
 import os
 import requests
 from dotenv import load_dotenv
+import logging
 
 PUSH_URL = 'https://api.pushover.net/1/messages.json'
 
 load_dotenv()
 PUSHOVER_USER = os.getenv('PUSHOVER_USER')
 PUSHOVER_TOKEN = os.getenv('PUSHOVER_TOKEN')
+
+logger = logging.getLogger('flightscraper')
 
 def push_notification(title, message, priority, retry=None, expire=None):
     data = {
@@ -25,4 +28,5 @@ def push_notification(title, message, priority, retry=None, expire=None):
 
     response = requests.post(PUSH_URL, data=data)
 
-    print(response)
+    if response.status_code != 200:
+        logger.error(f'Failed to send notification to pushover: {response}')
